39b926999d4270eb24ad545763ae73b19aa3d535
Losalini <C.Rokocakau@gmail.com>
2020-08-22 02:03:04 +1200

    Add incomplete bubble sort method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -12,17 +12,42 @@ def bubble_sort(array)
 #   return array
 # end
 
-# for i in 0..array.length-1
-# n = array[i]
-# x = array[i + 1]
-# until n <= x 
-#   n, x = x, n
-# end
-# return array[i]
-# end
 
-# print array[0], array[1] = array[1], array[0]
 
+
+=begin
+for i in 0..array.length-1
+   n = array[i]
+   x = array[i + 1]
+
+  until n <= x 
+   n, x = x, n
+  end 
+  p array[i]
+  #return array[i]
 end
+=end
+
+
+  
+
+
+  loop do
+    for i in 0..array.length - 1
+         value = array[i] <=> array[i + 1]
+
+         if value == 1
+         array[i], array[i + 1] = array[i + 1], array[i]
+         elsif value == 0 || value == -1
+          next
+        end
+    end
+    break if #breaks only if array is sorted; must return a boolean
+  end
+
+ puts "Array: #{array}"
+ #return array
+end
+ 
 
 p bubble_sort([2,3,7,4,1,6])
