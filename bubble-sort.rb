171126faf7e6fffb8d96b4dd3958d972ca6f55ae
Losalini <C.Rokocakau@gmail.com>
2020-08-22 03:08:59 +1200

    Add bubble_sort solution 1

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -27,22 +27,26 @@ end
 =end
 
 
-  # loop do
-  #   for i in 0..array.length - 1
-  #        value = array[i] <=> array[i + 1]
 
-  #        if value == 1
-  #        array[i], array[i + 1] = array[i + 1], array[i]
-  #        elsif value == 0 || value == -1
-  #         next
-  #       end
-  #   end
-    #breaks only if array is sorted; must return a boolean
-#     return array
-# end
+  sorted_array = array.sort
 
-#  puts "Array: #{array}"
- #return array
+   loop do
+     for i in 0..array.length - 1
+          value = array[i] <=> array[i + 1]
+
+           if value == 1
+             array[i], array[i + 1] = array[i + 1], array[i]
+           elsif value == 0 || value == -1
+             next
+          end
+      end
+     break if array == sorted_array 
+ end
+
+  puts "Array: #{array}"
+  
+  return array
+end
 
 #  =====================
 
@@ -61,6 +65,10 @@ end
 #   return array
 # end  
 
+
+#----------------------
+
+=begin
 arr = array.length - 1
 counter = 0
 
@@ -68,14 +76,19 @@ counter = 0
   if array[i] > array[i + 1]
     array[i], array[i + 1] = array[i + 1], array[i]
     counter = 1
-  elsif 
-    array[i] < array[-1]
+  elsif array[i] < array[-1]
+    puts "#{array[-1]}"
     counter == 0    
   break if 
     counter == 0
   end
+  
+  #puts "Array: #{array}"
   return array
   end
 
 end
-p bubble_sort([2,3,7,4,1,6])
+=end
+
+#p bubble_sort([2,3,7,4,1,6])
+p bubble_sort([100,100,30,2,9,5,1,-1,0,-190])
