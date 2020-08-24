000db884a9c010c18e001778e9b510753f95b3f1
Losalini <C.Rokocakau@gmail.com>
2020-08-24 20:08:48 +1200

    Edit bubble-sort solution

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,26 +1,44 @@
-def bubble_sort(array)
-
-  sorted_array = array.sort
-
-   loop do
-     for i in 0..array.length - 1
-          value = array[i] <=> array[i + 1]
-
-           if value == 1
-             array[i], array[i + 1] = array[i + 1], array[i]
-           elsif value == 0 || value == -1
-             next
-          end
-      end
-     break if array == sorted_array 
- end
-
-  puts "Array: #{array}"
-  
-  return array
+def bubble_sort_by(array)
+  yield
 end
+ 
+bubble_sort_by.each do |left, right|
+  value = left <=> right
+ 
+  if value == 1  
+    left, right = right, left
+  end
+end
+  
 
 #  =====================
+
+#def bubble_sort(array)
+
+#sorted_array = array.sort
+
+   #loop do
+     #for i in 0..array.length - 1
+        #value = array[i] <=> array[i + 1]
+
+        #if value == 1  
+          #array[i], array[i + 1] = array[i + 1], array[i]
+        #else
+          #next
+        #end
+
+      #end
+
+     #break if array == sorted_array 
+ #end
+
+ #puts "Array: #{array}"
+  
+  #return array
+#end
+
+#========================
+
 
 # arr = array.length - 1
 
@@ -42,5 +60,9 @@ end
 # end
 # end
 
+#===================
+
+
+
 #p bubble_sort([2,3,7,4,1,6])
-p bubble_sort([100,100,30,2,9,5,1,-1,0,-190])
+p bubble_sort_by([100,100,30,2,9,5,1,-1,0,-190])
